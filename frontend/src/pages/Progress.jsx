536efc3f635307b64bd6fc97750c15bc189c0de3
seongilhwang97 <hseongil7880@naver.com>
2025-02-09 23:23:52 +0900

    feat: progressCard added(image slide)

--- frontend/src/pages/Progress.jsx
+++ frontend/src/pages/Progress.jsx
@@ -1,7 +1,13 @@
+import BottomNavigationBar from "../components/Navigators/BottomNavigationBar";
+import TopNavigationBar from "../components/Navigators/TopNavigationBar";
+import ProgressCard from "../components/Progress/ProgressCard";
+
 export default function Progress() {
   return (
     <>
-      <a>progress page</a>
+    <TopNavigationBar/>
+    <ProgressCard/>
+    <BottomNavigationBar/>
     </>
   );
 }
