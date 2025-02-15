b23fe497ebf4414a1352c436be024b46ed7a321b
박성근 <qkrtjd50@naver.com>
2025-02-15 23:15:39 +0900

    refactor: refactor schedule-detail & review dummy datas

--- backend/src/main/java/com/ssafy/wevi/service/ScheduleService.java
+++ backend/src/main/java/com/ssafy/wevi/service/ScheduleService.java
@@ -29,6 +29,7 @@ public class ScheduleService {
     private final CategoryRepository categoryRepository;
     private final UserRepository userRepository;
     private final MiddleProcessStepRepository middleProcessStepRepository;
+    private final VendorService vendorService;
     // ========= 등록 ========//
     // 상담 등록
     @Transactional
@@ -705,6 +706,7 @@ public class ScheduleService {
         contractDto.setDetail(schedule.getDetail());
         contractDto.setCategoryId(schedule.getCategory().getId());
         contractDto.setCategoryName(schedule.getCategory().getName());
+        contractDto.setVendorImageUrl(vendorService.getImage(schedule.getVendor()).getImageUrl());
 
         return contractDto;
     }
@@ -735,6 +737,7 @@ public class ScheduleService {
         consultationDto.setRequest(consultation.getRequest());
         consultationDto.setCategoryId(consultation.getCategory().getId());
         consultationDto.setCategoryName(consultation.getCategory().getName());
+        consultationDto.setVendorImageUrl(vendorService.getImage(consultation.getVendor()).getImageUrl());
 
         return consultationDto;
     }
