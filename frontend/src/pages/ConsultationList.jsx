fe3340e5ec536396d6585982ef73fb775b92f878
Hwang Seong Il <hseongil7880@naver.com>
2025-02-17 15:23:06 +0900

    consultationList sort added

--- frontend/src/pages/ConsultationList.jsx
+++ frontend/src/pages/ConsultationList.jsx
@@ -3,14 +3,28 @@ import BottomNavigationBar from "../components/Navigators/BottomNavigationBar";
 import TopNavigationBar2 from "../components/Navigators/TopNavigationBar2";
 import { getConsultationList } from "../api/schedule";
 import ScheduleList from "../components/Schedule/ScheduleList";
-
+import dayjs from "dayjs";
 export default function ConsultationList() {
   const [consultationList, setConsultationList] = useState();
   useEffect(() => {
     const axiosConsultaionList = async () => {
       try {
         const consultationData = await getConsultationList();
-        setConsultationList(consultationData);
+        // ✅ `startDate`(날짜) → `startTime`(시간) 기준으로 정렬
+        const sortedData = consultationData.sort((a, b) => {
+          const dateA = dayjs(
+            `${a.startDate} ${a.startTime}`,
+            "YYYY-MM-DD HH:mm"
+          );
+          const dateB = dayjs(
+            `${b.startDate} ${b.startTime}`,
+            "YYYY-MM-DD HH:mm"
+          );
+
+          return dateA - dateB; // 오름차순 정렬
+        });
+
+        setConsultationList(sortedData);
       } catch (err) {
         console.log(err);
       }
@@ -24,7 +38,7 @@ export default function ConsultationList() {
   return (
     <>
       <TopNavigationBar2 title="상담 내역" />
-      <div className="h-[86vh]">
+      <div className="h-[86vh] overflow-auto ">
         {consultationList && consultationList.length > 0 ? (
           <ScheduleList
             selectedDate={0}
