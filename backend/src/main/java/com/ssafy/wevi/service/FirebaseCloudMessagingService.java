7c95646ff6877788a24dee76956a25bea080dcaa
신동운 <tlsehd5454@naver.com>
2025-02-20 12:54:51 +0900

    fix: FirebaseCloudMessagingService

--- backend/src/main/java/com/ssafy/wevi/service/FirebaseCloudMessagingService.java
+++ backend/src/main/java/com/ssafy/wevi/service/FirebaseCloudMessagingService.java
@@ -1,8 +1,6 @@
 package com.ssafy.wevi.service;
 
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,12 +12,14 @@ import java.util.concurrent.ExecutionException;
 @Slf4j
 public class FirebaseCloudMessagingService {
     public void sendPushNotification(String targetToken, String title, String body) {
+
         Message message = Message.builder()
-                .setToken(targetToken)
-                .setNotification(Notification.builder()
-                        .setTitle(title)
-                        .setBody(body)
+                .setWebpushConfig(WebpushConfig.builder()
+                        .setNotification(new WebpushNotification(
+                                title, body))
+                        .setFcmOptions(WebpushFcmOptions.withLink("http://localhost:5173/notification"))
                         .build())
+                .setToken(targetToken)
                 .build();
 
         try {
